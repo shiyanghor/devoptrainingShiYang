016576e0367c3314149630380e92d5551162efd5
shiyanghor <shi.yang.hor@intel.com>
2023-03-21 09:01:38 +0530

    updated line 17 in catalog.cs

--- CatalogAPI/Models/Catalog.cs
+++ CatalogAPI/Models/Catalog.cs
@@ -14,6 +14,7 @@ namespace CatalogAPI.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	//user2 make change
         [Required]
         [Column("Catalog_Id")]
         public long CatalogId { get; set; }
